f1d5f481bf4a6066deb3b8f8e688f9c3cd9b43d4
Shu X <shux@Shus-MacBook-Pro.local>
2026-05-15 17:37:43 +0800

    fix InstallBanner import

--- write-file.js
+++ write-file.js
@@ -4,9 +4,5 @@ content = content.replace(
   "import MobileHeader from './components/MobileHeader';",
   "import MobileHeader from './components/MobileHeader';\nimport InstallBanner from './components/InstallBanner';"
 )
-content = content.replace(
-  '<MobileHeader />',
-  '<MobileHeader />\n          <InstallBanner />'
-)
 fs.writeFileSync('app/layout.tsx', content)
 console.log('done')
